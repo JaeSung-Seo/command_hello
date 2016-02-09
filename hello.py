4344dde532404b7fd471e1c1140590211376fc29
JaeSung-Seo <hotshotseo@gmail.com>
2016-02-09 17:36:22 +0900

    new git change

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 // for command line git tutorial
 // github modifcation
 // to see what if
+// the whole new start
 print("Hello world")
 print("Tell your world")
 print("This is master branch adding")
